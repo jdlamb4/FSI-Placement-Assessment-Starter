9d4e615c2d7c7945175f02ac36022d5d5e924270
Jacob Lamb <jacoblamb15@gmail.com>
2025-04-13 17:59:03 -0400

    first button works

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,36 @@
 // HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+console.log("JavaScript code has loaded!");
 // First, tell us your name
-let yourName = "Jacob Lamb" // HINT: Replace this with your own name!
+let yourName = "Jacob Lamb"; // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+let gb = 0; // Gingerbread
+let cc = 0; // Chocolate Chip
+let sugar = 0; // Sugar Sprinkle
 
 // selecting the element with an id of credit
-const credit = document.querySelector('#credit')
+const credit = document.querySelector("#credit");
 // selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+const gbPlusBtn = document.querySelector("#add-gb");
 
 // Code to update name display
-credit.textContent = `Created by ${yourName}`
+credit.textContent = `Created by ${yourName}`;
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+gbPlusBtn.addEventListener("click", function () {
+  // HINT: You can delete this console.log after you no longer need it!
+  console.log("Gingerbread + button was clicked!");
+  gb++;
+  function updateGb(displayQuantity) {
+    let gbQty = document.querySelector("#qty-gb");
+    gbQty.textContent = displayQuantity;
+  }
+  gbQty = gb;
+  updateGb(
+    gbQty
+  ); /* I believe I don't need to do string interpolation because this ID is a number*/
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+  // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+});
 
 // TODO: Hook up event listeners for the rest of the buttons
